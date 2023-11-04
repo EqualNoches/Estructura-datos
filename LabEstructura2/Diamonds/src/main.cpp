1da7f2b8dcd48da1c882cbd1746514464d70419b
EqualNoches <119954229+EqualNoches@users.noreply.github.com>
2023-11-04 13:09:46 -0400

    Diamond completed

--- LabEstructura2/Diamonds/src/main.cpp
+++ LabEstructura2/Diamonds/src/main.cpp
@@ -13,18 +13,17 @@ int main() {
     for (int j = 0; j <= cantsps; j++) {
       cout << ("  ");
     }
-        // first pyramid asterisk
+    // first pyramid asterisk
     for (int k = 1; k <= 2 * i + 1; k++) {
       cout << ("* ");
     }
-	// spaces between
-	for (int j = 1; j>= 2*(size-1)+1;j++)
-	{
-		cout << ("::");
-	}
+    // spaces between
+    for (int j = 1; j <= 2 * (floors - i) + 1; j++) {
+      cout << ("  ");
+    }
 
-	//second pyramid
-	    for (int k = 1; k <= 2 * i + 1; k++) {
+    // second pyramid
+    for (int k = 1; k <= 2 * i + 1; k++) {
       cout << ("* ");
     }
 
@@ -35,13 +34,26 @@ int main() {
   // for the inverse of the triangle
   for (int i = floors - 1; i >= 0; i--) {
 
+    //spaces 
     for (int k = 0; k < floors - i; k++) {
       cout << ("  ");
     }
 
+    //asterisks
     for (int j = 1; j <= 2 * i + 1; j++) {
       cout << ("* ");
     }
+
+        // spaces between
+    for (int j = 1; j <= 2 * (floors - i) + 1; j++) {
+      cout << ("  ");
+    }
+
+        //asterisks
+    for (int j = 1; j <= 2 * i + 1; j++) {
+      cout << ("* ");
+    }
+
 
     cout << endl;
   }
