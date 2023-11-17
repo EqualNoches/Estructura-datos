cc483c04a03872d161b8458b20c97a24f2ab82f3
EqualNoches <119954229+EqualNoches@users.noreply.github.com>
2023-11-16 20:27:24 -0400

    diamonds version casimiro

--- LabEstructura2/diamonds/src/main.cpp
+++ LabEstructura2/diamonds/src/main.cpp
@@ -2,80 +2,46 @@
 
 using namespace std;
 
+void espacio(int n) {
+    for (int i = 0; i < n; i++) {
+        cout << " ";
+    }
+}
+
+void rombos(int t) {
+    int x, y, k, suc, i = 0;
+
+    for (x = 0; x < t; x++) {
+        if (x < t / 2) {
+            k = t / 2 - x;
+            espacio(k);
+        }
+        else {
+            k = x - t / 2;
+            espacio(k);
+        }
+        suc = 2 * i + 1;
+        for (y = 0; y < 2 * suc; y++) {
+            cout << "*";
+
+            if (y == suc - 1) {
+                espacio(2 * k); 
+            }
+        }
+        if (x < t / 2) {
+            i++;
+        }
+        else {
+            i--;
+        }
+        cout << endl;
+    }
+}
+
 int main() {
+    int size = 9;
 
-  int floors = 5;
+    rombos(size);
 
-  int cantsps = floors - 1;
-
-  // which floor
-  for (int i = 0; i <= floors; i++) {
-
-    // first spaces
-    for (int j = 0; j <= cantsps; j++) {
-
-      cout << ("  ");
-    }
-
-    // first pyramid asterisk
-    for (int k = 1; k <= 2 * i + 1; k++) {
-
-      cout << ("* ");
-    }
-
-    // spaces between
-
-    for (int j = 1; j <= 2 * (floors - i) + 1; j++) {
-
-      cout << ("  ");
-    }
-
-    // second pyramid
-
-    for (int k = 1; k <= 2 * i + 1; k++) {
-
-      cout << ("* ");
-    }
-
-    cout << endl;
-
-    cantsps--;
-  }
-
-  // for the inverse of the triangle
-
-  for (int i = floors - 1; i >= 0; i--) {
-
-    // spaces
-
-    for (int k = 0; k < floors - i; k++) {
-
-      cout << ("  ");
-    }
-
-    // asterisks
-
-    for (int j = 1; j <= 2 * i + 1; j++) {
-
-      cout << ("* ");
-    }
-
-    // spaces between
-
-    for (int j = 1; j <= 2 * (floors - i) + 1; j++) {
-
-      cout << ("  ");
-    }
-
-    // asterisks
-
-    for (int j = 1; j <= 2 * i + 1; j++) {
-
-      cout << ("* ");
-    }
-
-    cout << endl;
-  }
-
-  return 0;
+    return 0;
 }
