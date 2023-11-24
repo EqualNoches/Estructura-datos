7b7cb7bff08ed7fcbc9417c64be7c68f48bd1f68
EqualNoches <119954229+EqualNoches@users.noreply.github.com>
2023-11-24 19:54:29 -0400

    Agragado codigo de conversión de numeros

---
+++ LabEstructura2/entero-decimal-hexadecimal/main.cpp
@@ -0,0 +1,59 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string convertDecimalBinary(int number)
+{
+
+    int quotient, binaryNum = 0;
+    string binaryOut;
+    quotient = number;
+    while (quotient != 0)
+    {
+        int remainder;
+        string temp = " ";
+        remainder = quotient % 2;
+        quotient /= 2;
+
+        temp = to_string(remainder);
+        binaryOut = temp + binaryOut;
+    }
+
+    return binaryOut;
+}
+
+string convertBinaryHex(int decimal)
+{
+    int  hexNumber = 0;
+    string hexLetter;
+    while (decimal != 0)
+    {
+        int temp;
+        temp = (decimal % 16);
+        (temp < 10) ? temp += 48: temp += 55; 
+        hexNumber = hexNumber * 100 + temp;
+        decimal /= 16;
+    }
+    hexLetter = hexNumber;
+    
+    
+    return hexLetter;
+}
+
+int main(int argc, char *argv[])
+{
+
+    int decimal;
+    string binaryStr, hexadecimal;
+
+    cout << "Please type a decimal number: ";
+    cin >> decimal;
+
+    binaryStr = convertDecimalBinary(decimal);
+    hexadecimal = convertBinaryHex(decimal);
+    cout << "The number " << decimal << " is converted to binary like this: :" << binaryStr << endl;
+    cout << "The number : " << decimal << " is converted to Hex code like this: " << hexadecimal << endl;
+    return 0;
+}
