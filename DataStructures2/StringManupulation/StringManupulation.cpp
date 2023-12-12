3b591322167d94b450418a67d02c918f043a4c03
EqualNoches <1115808@est.intec.edu.do>
2023-12-12 17:15:27 -0400

    changes

--- DataStructures2/StringManupulation/StringManupulation.cpp
+++ DataStructures2/StringManupulation/StringManupulation.cpp
@@ -3,30 +3,40 @@
 #include <string>
 
 std::string string_reversing(std::string word);
+void ValidarCadena(std::string s);
+
 int main(int argc, char const *argv[])
 {
     // TODO: make a program in which a certain file inside a directory, each wordk will be changed backwards
-    std::string test;
-    std::cin >> test;
+    std::string inputNameFile, outputNameFile;
 
-    std::string word = string_reversing(test);
-    std::cout << word << std::endl;
+    std::cout << "Porfavor ingrese el nombre del archivo. \nNo ponga el tipo de extension(.txt)\n(Este programa solo con archivos txt) \n Nombre: ";
+    ValidarCadena(inputNameFile);
+    inputNameFile.append(".txt");
+
+    std::cout << inputNameFile << std::endl;
+    std::ifstream inputFile("input.txt");
+    // std::ofstream outputFile("output.txt");
 
     return 0;
 }
 
 std::string string_reversing(std::string word)
 {
-    int size_word = sizeof(word);
-    std::string reversed_word = " ";
-    for (int i = size_word; i < 0; i++)
+    std::string reversed;
+    for (int i = word.length() - 1; i >= 0; --i)
     {
-        while (word[i] != ' ')
-        {
-
-            reversed_word += word[i];
-        }
+        reversed += word[i];
     }
+    return reversed;
+}
 
-    return reversed_word;
+void ValidarCadena(std::string s)
+{
+    while (!(std::cin >> s))
+    {
+        std::cout << "Porfavor introduzca un valor valido";
+        std::cin.clear();
+        std::cin.ignore(123, '\n');
+    }
 }
