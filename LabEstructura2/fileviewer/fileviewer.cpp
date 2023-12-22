2578e4ba1a81846f2e692ab58d854217c6f8d4cd
EqualNoches <119954229+EqualNoches@users.noreply.github.com>
2023-12-21 21:32:22 -0400

    update fileviewer

--- LabEstructura2/fileviewer/fileviewer.cpp
+++ LabEstructura2/fileviewer/fileviewer.cpp
@@ -1,32 +1,53 @@
 #include <iostream>
+#include <stdio.h>
+#include <sys/stat.h>
 #include <dirent.h>
+#include <fstream>
+#include <iomanip>
 
-int main(int argc, char const *argv[])
-{
-    // TODO: Make a program in which you can see the contents of the actual directory
-    // Open the current directory
-    DIR *dir = opendir(".");
-    
-    // Check if the directory was opened successfully
-    if (dir == NULL) {
-        perror("Error opening directory");
-        return 1;
+using namespace std;
+
+void ls(const char *dir) {
+  DIR *handle = opendir(dir);
+  cout << "Directory of " << dir << endl;
+  if (handle == NULL) {
+    return;
+  }
+  struct dirent *entry;
+  int file_count = 0;
+  int total_bytes = 0;
+
+  while ((entry = readdir(handle)) != NULL) {
+    string name = entry->d_name;
+    if (name == "." || name == "..") {
+      continue;
     }
 
-    // Read and print directory entries
-    struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL) {
-        // Ignore "." and ".." entries
-        if (entry->d_name[0] == '.') {
-            continue;
-        }
-
-        // Print the name of the file or directory
-        printf("%s\n", entry->d_name);
+    FILE *fp = fopen(entry->d_name, "r");
+    if (fp == NULL) {
+      continue;
     }
 
-    // Close the directory
-    closedir(dir);
+    int file_size_in_bytes = 0;
+    fseek(fp, 0, SEEK_END);
+    file_size_in_bytes = ftell(fp);
+    fclose(fp);
+    total_bytes += file_size_in_bytes;
 
-    return 0;
+    struct stat st;
+    stat(entry->d_name, &st);
+    struct tm *timeinfo = localtime(&st.st_mtime);
+     cout << put_time(timeinfo, "%Y/%m/%d %H:%M") << "\t" << file_size_in_bytes
+             << "\t" << entry->d_name << endl;
+         
+    file_count++;
+  }
+  cout << "\t " << file_count << " files(s) \t " << total_bytes << " bytes"
+       << endl;
+  closedir(handle);
+}
+
+int main(void) {
+  ls("./");
+  return 0;
 }
