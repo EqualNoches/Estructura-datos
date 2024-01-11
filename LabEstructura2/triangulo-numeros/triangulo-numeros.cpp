ba38d9644f2ed3d844f33235b01090913175fa3a
EqualNoches <119954229+EqualNoches@users.noreply.github.com>
2024-01-11 15:29:38 -0400

    triangulo numeros

--- LabEstructura2/triangulo-numeros/triangulo-numeros.cpp
+++ LabEstructura2/triangulo-numeros/triangulo-numeros.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
 #include <regex>
 
-struct Node{
-    int data;
-    Node *next;
-};
-
-void Push(Node *&stack, int n)
-{
-    Node *new_node = new Node();
-    new_node->data = n;
-    new_node->next = stack;
-    stack = new_node;
-    std::cout << "\nElemento " << n << " agregado a la pila correctamente\n" << std::endl;
-}
 
 bool isNumber(const std::string& input){
     std::regex pattern("^[0-9]+$");
@@ -22,15 +9,18 @@ bool isNumber(const std::string& input){
 
 
 int main(int argc, char* argv[]){
-    int input_numbers;
-    std::cout << "Introduzca los 51 numeros que se van a introducir";
-    for (int i = 0; i < 52; i++)
+    std::string input_number= "0";
+    int number;
+    int aux;
+    std::cout << "Introduzca un numero mayor a 55"<< std::endl;
+    do{
+        std::cout << "Numero: ";
+        std::cin >> input_number;
+    }while(!isNumber(input_number));
+    number = std::stoi(input_number);
+    for (int i = 0; i < 55; i++)
     {
-        do
-        {
-        std::cout << "Numero "<< i;
-        std::cin >> input_numbers;
-        }while(isNumber(input_numbers));
+
     }
     return 0;
 }
